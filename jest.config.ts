e7ed74a461a9f3b61732d4c6c9d54c1b552ca26c
Rubén Medellín <ruben.medellin@gmail.com>
2024-12-12 02:53:25 -0600

    Fixed test suite

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,13 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  transform: {
+    "\\.[jt]sx?$": ["ts-jest", { useESM: true }]
+  },
+  moduleNameMapper: {
+    "(.+)\\.js$": "$1",
+  },
+  extensionsToTreatAsEsm: [".ts"],
 };
 
 export default config;
--- tests/WFC.test.ts
+++ tests/WFC.test.ts
@@ -1,10 +1,11 @@
 import { TileDef } from "../src/TileDef";
 import { WFC } from "../src/WFC";
+import { SquareGrid } from "../dist/Grid";
 
 describe("WFC", () => {
   it("should initialize with default options", () => {
     const tileDefs: TileDef[] = [];
-    const wfc = new WFC(tileDefs);
+    const wfc = new WFC(tileDefs, new SquareGrid(1, 1));
 
     expect(wfc).toBeInstanceOf(WFC);
   });
